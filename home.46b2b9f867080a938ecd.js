ea596615856cf9e1751ef854696fa06d9d097f5c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-21 07:19:09 +0000

    Deploy dogodo-cc/on-the-way to github.com/dogodo-cc/on-the-way.git:gh-pages

---
+++ home.46b2b9f867080a938ecd.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{156:function(e,n,t){"use strict";t.r(n);var i=function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n  相信我，等我有时间了，会把这个网站弄好看的。\n")])};i._withStripped=!0;var s={name:"home"},o=t(12),r=Object(o.a)(s,i,[],!1,null,null,null);r.options.__file="src/views/home/index.vue";n.default=r.exports}}]);
