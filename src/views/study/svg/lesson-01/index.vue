f026371e6c3291249d7ce6f744e5284d3ddc438f
yuanshuai <yuanshuai@gaoding.com>
2019-07-21 17:07:44 +0800

    delete svg xml

--- src/views/study/svg/lesson-01/index.vue
+++ src/views/study/svg/lesson-01/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="page">
     <div style="width: 200px;height: 200px;outline:1px solid #333;margin: 50px auto;">
-    <?xml version="1.0" standalone="no"?>
-
-    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
 
     <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
 
@@ -33,10 +30,7 @@
 
 </div>
 <div style="width: 600px;height: 300px;outline:1px solid #333;margin: 50px auto;">
-    <?xml version="1.0" standalone="no"?>
-
-    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-
+   
     <svg width="100%" height="100%" version="1.1" xmlns="http://www.w3.org/2000/svg">
         <ellipse cx="154" cy="154" rx="150" ry="120" style="fill:#999999"></ellipse>
         <ellipse cx="152" cy="152" rx="150" ry="120" style="fill:#cceeff"></ellipse>
