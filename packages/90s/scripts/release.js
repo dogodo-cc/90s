c967df1f52171a66094769c4413cbc64f26dfd85
yuanshuai <yuanshuai@gaoding.com>
2019-08-26 12:52:53 +0800

    chore(构建): 使用release.js

--- packages/90s/scripts/release.js
+++ packages/90s/scripts/release.js
@@ -1,5 +1,18 @@
 /* eslint-disable no-undef */
 
-cp('./CNAME' ,'./dist');
-cp('./public/favicon.ico', './dist');
-cp('./website/*', './dist');
+const path = require("path");
+const exec = require("child-process-promise").exec;
+const projectPath = path.join(__dirname, "../");
+
+exec('cp ./CNAME ./dist', {
+  cwd: projectPath
+})
+exec('cp ./public/favicon.ico ./dist', {
+  cwd: projectPath
+})
+exec('cp -R ./website/* ./dist', {
+  cwd: projectPath
+}).then(() => {
+  console.log('😄静态文件复制完成！'); //eslint-disable-line
+})
+
