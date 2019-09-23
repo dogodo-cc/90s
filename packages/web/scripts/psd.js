265aea5abee1073a8e47cae273d0969c447c47ac
yuanshuai <yuanshuai@gaoding.com>
2019-09-23 14:13:06 +0800

    feat(学习笔记): psd.js

---
+++ packages/web/scripts/psd.js
@@ -0,0 +1,27 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+
+// node packages/web/scripts/psd.js
+
+const PSD = require('psd');
+const fs = require('fs');
+const path = require('path');
+const psdPath = path.join(__dirname, 'test.psd');
+const psd = PSD.fromFile(psdPath);
+psd.parse();
+const treeData = psd.tree().export()
+
+fs.writeFile(path.join(__dirname, 'psd.json'), JSON.stringify(treeData), 'utf-8', err => {
+  if (err) {
+    console.log(err)
+  }else {
+    console.log('create psd.json file done!');
+  }
+})
+// console.log(psd.tree().childrenAtPath('A/B/C')[0].export());
+
+PSD.open(psdPath).then(function (psd) {
+  return psd.image.saveAsPng(path.join(__dirname, './psd.png'));
+}).then(function () {
+  console.log("create a psd.png done!");
+});
