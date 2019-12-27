f261116ac6be84d9b86022a59bb317d87279d8a9
yuanshuai <yuanshuai@gaoding.com>
2019-12-27 15:42:30 +0800

    style(学习): delete some useless files

--- packages/web/src/route/index.js
+++ packages/web/src/route/index.js
@@ -153,15 +153,6 @@ const router = new VueRouter({
     },
     {
       path: '/sharon',
-      component:() => import('@/views/sharon/photos/index.vue'),
-      meta: {
-        title: '照片墙',
-        pageTitle: '我们结婚啦^_^',
-        hidden: true
-      }
-    },
-    {
-      path: '/love',
       component:() => import('@/views/sharon/love/index.vue'),
       meta: {
         title: '爱心表白',
