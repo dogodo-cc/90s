c847210d2219a99232fa5cf52ceea8f58be20b9f
yuanshuai <yuanshuai@gaoding.com>
2020-01-13 20:17:36 +0800

    feat(学习笔记): 抽奖

--- packages/web/src/views/note/draw/turntable.vue
+++ packages/web/src/views/note/draw/turntable.vue
@@ -1,7 +1,6 @@
 <template>
 <div class="uxms-draw">
-  <el-switch class="switch" v-model="useAudio"/>
-  <div class="title">设计工坊&哥伦布尾牙抽奖</div>
+  <img class="header" @click="useAudio = !useAudio" src="./images/head.png" alt="">
   <div class="draw-turntable">
     <div
       class="turntable"
@@ -10,19 +9,24 @@
         transition: `transform ${time}ms cubic-bezier(0, .31, .57, 1.03)`
       }"
       @transitionend="isPlaying = false">
-      <div
-        :class="['piece', `piece-${index}`]"
-        v-for="(item, index) in pieces"
-        :key="index"
-        >
-        <div class="content">
-          <span>{{item}}</span>
+      <template v-if="false">
+        <div
+          :class="['piece', `piece-${index}`]"
+          v-for="(item, index) in pieces"
+          :key="index"
+          >
+          <div class="content">
+            <span>{{item}}</span>
+          </div>
         </div>
-      </div>
+      </template>
     </div>
-    <div class="start" @click="start"></div>
+    <div class="start" @click="start">
+      <img src="./images/btn.png" alt="">
+    </div>
   </div>
   <audio preload ref="audio" src="https://st-gdx.dancf.com/assets/20200111-170134-3df4.mov"></audio>
+  <img class="footer" src="./images/foot.png" alt="">
 </div>  
 </template>
 
@@ -31,7 +35,7 @@ export default {
   name: 'draw',
   data() {
     return {
-      pieces: ['冻结','冰粉','王导','再来', '元帅','腰果','冻结','希罗','童童','坐标','焦糖','牛仔'],
+      pieces: ['牛仔', '焦糖','冻结','元帅','腰果','希罗','再来','童童','坐标','冰粉','再来','王导'],
       radis: 30,
       time: 0,
       totalDeg: 0,
@@ -56,7 +60,7 @@ export default {
         this.totalDeg = 360 * 8 + this.radis * random + this.radis / 2;
         this.time = 10 * 1000;
         this.playAudio();
-        console.log(this.pieces[random], random);// eslint-disable-line
+        console.log(this.pieces.resever()[random], random);// eslint-disable-line
       }, 100)
     },
     playAudio() {
@@ -72,61 +76,36 @@ export default {
 
 <style lang="scss">
 .uxms-draw {
-  padding-top: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   overflow: hidden;
-  .title {
-    font-size: 20px;
-    padding: 20px 0;
-    text-align: center;
-  }
-  .switch {
+  height: 100vh;
+  background: url('./images/bg.png') top center no-repeat;
+  background-size: cover;
+  .header {
     position: fixed;
-    left: 0;
+    top: 0;
+    max-width: 100%;
+  }
+  .footer {
+    position: fixed;
     bottom: 0;
+    max-width: 100%;
   }
 }
 .draw-turntable {
   position: relative;
   width: 300px;
   height: 300px;
-  margin: 30px auto;
 
   .turntable {
     position: relative;
     width: 100%;
     height: 100%;
-    left: 50%;
-    transform-origin: left center;
-    .piece {
-      position: absolute;
-      left: 0;
-      top: 0;
-      width: 150px;
-      height: 300px;
-      border-radius: 0 150px 150px 0;
-      transform-origin: left center;
-      .content {
-        width: 50%;
-        height: 50%;
-        transform: rotate(15deg);
-        transform-origin: left top;
-        text-align: center;
-        span {
-          color: #fff;
-        }
-      }
-      &:nth-child(even) {
-        background-image: linear-gradient(120deg, green,green 75px, transparent 75px);
-      }
-      &:nth-child(odd) {
-        background-image: linear-gradient(120deg, #ccc, #ccc 75px, transparent 75px);
-      }
-    }
-    @for $index from 0 to 11 {
-      .piece-#{$index} {
-        transform: rotate(-30deg * ($index + 1));
-      }
-    }
+    background: url('./images/turntable.png') center center no-repeat;
+    background-size: cover;
+    transform-origin: center center;
   }
   .start {
     width: 50px;
@@ -138,18 +117,10 @@ export default {
     top: 50%;
     margin-left: -25px;
     margin-top: -25px;
-    &::after {
-      display: block;
-      position: absolute;
-      left: 50%;
-      top: -18px;
-      margin-left: -10px;
-      content: '';
-      width: 0;
-      height: 0;
-      border-left: 10px solid transparent;
-      border-right: 10px solid transparent;
-      border-bottom: 20px solid #fff;
+    img {
+      max-width: 100%;
+      width: 100%;
+      margin-top: -9px;
     }
   }
 }
