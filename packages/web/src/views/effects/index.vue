4cd764b70e1bae8a324287104f85b14fc6c5d5e0
yuanshuai <yuanshuai@gaoding.com>
2020-03-20 10:57:25 +0800

    feat(博客系统): add time

--- packages/web/src/views/effects/index.vue
+++ packages/web/src/views/effects/index.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="effect-page">
     <SvgPath></SvgPath>
-    <StarBg></StarBg>
+    <!-- <StarBg></StarBg> -->
+    <HoverPress>
+      <img width="400" height="300" src="https://cdn.dancf.com/uxms/www/image/3-5_e8021f69.png" alt="">
+    </HoverPress>
   </div>
 </template>
 
 <script>
 import SvgPath from './components/svg-path/index.vue';
-import StarBg from './star/index.vue'
+import HoverPress from './components/hover-press/index.vue';
+// import StarBg from './star/index.vue'
 export default {
   name: 'effects',
   components: {
     SvgPath,
-    StarBg
+    // StarBg,
+    HoverPress
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 .effect-page {
   height: 100vh;
-  background-color: #000;
+  // background-color: #000;
 }
 </style>
