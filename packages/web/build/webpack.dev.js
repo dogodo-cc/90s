8b8b663764661e74113a66fd6366d868a8b89846
yuanshuai <yuanshuai@gaoding.com>
2020-05-24 16:06:05 +0800

    feat: vuepress to docsify

--- packages/web/build/webpack.dev.js
+++ packages/web/build/webpack.dev.js
@@ -14,6 +14,7 @@ module.exports = merge(common, {
     contentBase: path.resolve(__dirname, '../dist'),
     port: 8040,
     hot: true,
+    disableHostCheck: true, // 如果配置了 nginx 90s.com 会报错 Invalid Host header
     stats: "minimal"
   }
 })
